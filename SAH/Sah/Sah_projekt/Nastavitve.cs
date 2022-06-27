4ca38478c97f4c54d0b9472c9fea3e911928f54e
titoo1234 <tito.arnsek@gmail.com>
2022-06-27 18:55:58 +0200

    Nastavitve

--- SAH/Sah/Sah_projekt/Nastavitve.cs
+++ SAH/Sah/Sah_projekt/Nastavitve.cs
@@ -6,33 +6,96 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 using System.Windows.Forms;
+using System.ComponentModel;
 
 namespace Sah_projekt
 {
     public partial class Nastavitve : Form
     {
+        private Socket socket;
+        private BackgroundWorker messageReceiver = new BackgroundWorker();
+        private TcpListener server = null;
+        private TcpClient client;
         private Color[] tema;
         private Size velikost;
         private string barva;
         private Game game;
         private int cas;
         private string nacinIgre;
-
-        public Nastavitve(string nacinIgre)
+        private string ipNaslov;
+        public Nastavitve(string nacinIgre,string ip = null)
         {
+            
             InitializeComponent();
+            PrivzeteNastavitve();
+            this.NacinIgre = nacinIgre;
+            this.IpNaslov = ip;
+            VzpostaviPovezavo();
+            
+        }
+        /// <summary>
+        /// funkcija vzpostavi povezavo med igralcema
+        /// </summary>
+        private void VzpostaviPovezavo()
+        {
+            if (NacinIgre == "HOST")
+            {
+                this.MessageReceiver = new BackgroundWorker();
+                //MessageReceiver.DoWork += MessageReceiver_DoWork;
+                server = new TcpListener(System.Net.IPAddress.Any, 5732);
+                server.Start();
+                Socket = server.AcceptSocket();
+            }
+            if (NacinIgre == "GOST")
+            {
+                label1.Visible = false;
+                label3.Visible = false;
+                BelaBarva_Gumb.Visible = false;
+                CrnaBarva_gumb.Visible = false;
+                IzberiCas.Visible = false;
+                this.IpNaslov = "localhost";
+                ZacetekIgre.Enabled = false;
+                this.MessageReceiver = new BackgroundWorker();
+                //MessageReceiver.DoWork += MessageReceiver_DoWork;
+                try
+                {
+                    client = new TcpClient(IpNaslov, 5732);
+                    Socket = client.Client;
+                    MessageReceiver.RunWorkerAsync();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    this.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Funkcija nastavi privzete nastavitve od strani
+        /// </summary>
+        private void PrivzeteNastavitve()
+        {
             BelaBarva_Gumb.Enabled = false;
             temaSahovnicaGumb1.Enabled = false;
             IzberiCas.SelectedIndex = 2;
-            this.NacinIgre = nacinIgre;
+
         }
+
+        public Socket Socket { get; set; }
+        public BackgroundWorker MessageReceiver { get; set; }
+        public object Server { get; set; }
+        public object Client { get; set; }
         public Game Game { get; set; }
         public Color[] Tema { get; set; }
         public Size Velikost { get; set; }
         public string Barva { get; set; }
         public int Cas { get; set; }
         public string NacinIgre { get; set; }
+        public string IpNaslov { get; set; }
 
         private void label2_Click(object sender, EventArgs e)
         {
@@ -48,22 +111,55 @@ namespace Sah_projekt
         {
             CrnaBarva_gumb.Enabled = false;
             BelaBarva_Gumb.Enabled = true;
-            //Image slika = CrnaBarva_gumb.Image;
             IzbranaBarva.Image = CrnaBarva_gumb.BackgroundImage;
-            //IzbranaBarva.SizeMode = PictureBoxSizeMode.StretchImage;
         }
 
         private void ZacetekIgre_Click(object sender, EventArgs e)
         {
-            string barvaFigur;
-            Color[] barve;
-
+            NastaviBarvo();
+            NastaviTemo();
+            this.Cas = int.Parse((string)IzberiCas.SelectedItem);
+            this.Game = new Game(true, false, false);
+            this.Velikost = new Size(40, 40);
+            NastaviIgro();
+            ZacniIgro();
+        }
+        /// <summary>
+        /// Funkcija nastavi zacetno barvo sahovnice
+        /// </summary>
+        private void NastaviBarvo()
+        {
             if (BelaBarva_Gumb.Enabled == false) this.Barva = "W";
             else this.Barva = "B";
-            
+        }
+        /// <summary>
+        /// Funkcija nastavi način igre, ki ga bomo igrali
+        /// </summary>
+        private void NastaviIgro()
+        {
+            if (this.NacinIgre == "SOLO") this.Game.Igra = new SoloIgra(this);
+            else if (this.NacinIgre == "RACUNALNIK") this.Game.Igra = new RacunalnikIgra(this);
+            else this.Game.Igra = new MultiplayerIgra(this);
+        }
+        /// <summary>
+        /// Funkcija začne igro
+        /// </summary>
+        private void ZacniIgro()
+        {
+            Visible = false;
+            //POŠLJEMO NASPORTONIKU
+            if (!this.Game.IsDisposed)
+                this.Game.ShowDialog();
+            Visible = true;
+        }
+        /// <summary>
+        /// Funkcija nastavi temo igre
+        /// </summary>
+        private void NastaviTemo()
+        {
             if (temaSahovnicaGumb1.Enabled == false)
             {
-                this.Tema = barve = new Color[] { Color.White, Color.Green, Color.Yellow };
+                this.Tema  = new Color[] { Color.White, Color.Green, Color.Yellow };
             }
             else
             {
@@ -71,19 +167,6 @@ namespace Sah_projekt
                 System.Drawing.Color col1 = System.Drawing.ColorTranslator.FromHtml("#fccc74");
                 this.Tema = new Color[] { col1, col, Color.Yellow };
             }
-            this.Cas = int.Parse((string)IzberiCas.SelectedItem);
-            this.Game = new Game(true, false, false);
-            Console.WriteLine(Game.Size);
-            this.Velikost = new Size(40, 40);
-
-            if (this.NacinIgre == "SOLO") this.Game.Igra = new SoloIgra(this);
-            else if (this.NacinIgre == "RACUNALNIK") this.Game.Igra = new RacunalnikIgra(this);
-            else this.Game.Igra = new SoloIgra(this);
-
-            Visible = false;
-            if (!this.Game.IsDisposed)
-                this.Game.ShowDialog();
-            Visible = true;
         }
 
         private void temaSahovnicaGumb1_Click(object sender, EventArgs e)
